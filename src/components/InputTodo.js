881c4f8c1378e45a7127a8d7e60bc7a064b8ea92
Gaby <gaby.es.iaa@gmail.com>
2022-02-14 18:09:58 -0500

    Create InputTodo component

---
+++ src/components/InputTodo.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { AiOutlineEnter } from 'react-icons/ai';
+
+const InputTodo = ({ addTask }) => {
+  const [title, setTitle] = useState('');
+  const changeField = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim() !== '') {
+      addTask(title.trim());
+      setTitle('');
+    }
+  };
+  return (
+    <form className="todo__form" onSubmit={handleSubmit}>
+      <input type="text" placeholder="Add todo..." onChange={changeField} value={title} />
+      <button type="submit"><AiOutlineEnter /></button>
+    </form>
+  );
+};
+
+InputTodo.propTypes = {
+  addTask: PropTypes.func.isRequired,
+};
+
+export default InputTodo;
